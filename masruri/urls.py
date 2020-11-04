15f2831c8f8e93186900cbadb468f673a0449d6d
ackerman <do.crazy192@gmail.com>
2020-11-04 20:40:09 +0700

    add urls app

---
+++ masruri/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('products/', views.products, name='products'),
+    path('custumer/', views.custumer, name='custumer'),
+    path('student/', views.student, name='student'),
+]
