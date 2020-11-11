f367de32148bd662a71677eb4bb8a0aa9eba577d
ackerman <do.crazy192@gmail.com>
2020-11-11 15:17:54 +0700

    register all models in admin

--- masruri/admin.py
+++ masruri/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from . models import Student
+from . models import *
 
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(Custumer)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Tag)
