3a8ac0a7e9badd0d67877b63c8ca0b147434f58e
Hafet17 <do.crazy192@gmail.com>
2020-12-04 14:54:03 +0700

    add str in urls

--- masruri/urls.py
+++ masruri/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('products/', views.products, name='products'),
-    path('custumer/', views.custumer, name='custumer'),
+    path('custumer/<str:pk>', views.custumer, name='custumer'),
     path('student/', views.student, name='student'),
 ]
