c4180a13e0f4a8b490723b7bd1de03fae6e2e20c
Hafet17 <do.crazy192@gmail.com>
2020-12-08 23:15:26 +0700

    return name in models product

--- masruri/models.py
+++ masruri/models.py
@@ -68,4 +68,4 @@ class Order(models.Model):
     status = models.CharField(max_length=200, null=True, choices=STATUS)
 
     def __str__(self):
-        return '%s, %s' % (self.status, self.custumer)
+        return self.product.name
