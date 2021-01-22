3365eaac58019c3ef879b8c09a5f7a943d10fbf3
Hafet17 <do.crazy192@gmail.com>
2021-01-22 16:14:34 +0700

    move views lah

--- masruri/views.py
+++ masruri/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from . models import *
 from . forms import *
 from django.contrib import messages
 from django.core.paginator import Paginator
-from django.db.models import Q
+from masruri.filters import OrderFilter
+
 
 # Create your views here.
-
 
 def home(request):
 
@@ -31,98 +29,10 @@ def home(request):
 
 # ========== views Product ============
 
-def products(request):
-
-    search_product = request.GET.get('q')
-
-    if search_product:
-        product = Product.objects.filter(name=search_product)
-    else:
-        # If not searched, return default posts
-        product = Product.objects.all().order_by("-date_created")
-
-    paginator = Paginator(product, 6)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-
-    data = {
-        'judul': 'Halaman Produk',
-        'menu': 'products',
-        'page_obj': page_obj
-    }
-    return render(request, 'product/index.html', data)
-
-
-def createProducts(request):
-
-    form = ProductForm()
-    if request.method == 'POST':
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(
-                request, "Product baru berhasil ditambahkan")
-            return redirect('/products')
-
-    data = {
-        'judul': 'Create Data Product',
-        'form': form
-    }
-
-    return render(request, 'product/product_form.html', data)
-
-
-def updateProducts(request, pk):
-
-    product = Product.objects.get(id=pk)
-    form = ProductForm(instance=product)
-    if request.method == 'POST':
-        form = ProductForm(request.POST, instance=product)
-        if form.is_valid():
-            form.save()
-            messages.success(
-                request, "Product Berhasil Di Update")
-            return redirect('/products')
-
-    data = {
-        'judul': 'Update Data Product',
-        'form': form
-    }
-
-    return render(request, 'product/product_form.html', data)
-
-
-def deleteProducts(request, pk):
-
-    delete_product = Product.objects.get(id=pk)
-    if request.method == 'POST':
-        delete_product.delete()
-        messages.success(
-            request, "Product Berhasil Di Hapus")
-        return redirect('/products')
-
-    data = {
-        'judul': 'Delete Data Product',
-        'deleteproduct': delete_product
-    }
-    return render(request, 'product/delete_form.html', data)
 
 
 # ========== views custumer ===========
-def custumer(request, pk):
 
-    detailcustumer = Custumer.objects.get(id=pk)
-    order_custumer = detailcustumer.order_set.all()
-    total_custumer = order_custumer.count()
-
-    data = {
-        'judul': 'Halaman Customer',
-        'menu': 'custumer',
-        'custumer': detailcustumer,
-        'order_custumer': order_custumer,
-        'total_custumer': total_custumer
-    }
-    return render(request, 'customer.html', data)
 
 
 def student(request):
