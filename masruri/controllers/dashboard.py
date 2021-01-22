d1696bb480d051a4d0f255cd7efb811d8475b5fd
Hafet17 <do.crazy192@gmail.com>
2021-01-22 16:11:58 +0700

    create controllers dashboard

---
+++ masruri/controllers/dashboard.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from masruri.forms import *
+from django.contrib.auth.decorators import login_required
+from masruri.decorators import tolakhalaman_ini, ijinkan_pengguna, pilihan_login
+
+
+@login_required(login_url='login')
+@ijinkan_pengguna(yang_diizinkan=['admin'])
+@pilihan_login
+def home(request):
+
+    list_custumer = Custumer.objects.all()
+    list_order = Order.objects.all()
+    total_order = list_order.count()
+    delivered = list_order.filter(status='Deliverd').count()
+    pending = list_order.filter(status='Pending').count()
+
+    data = {
+        'judul': 'Halaman Dashboard',
+        'menu': 'home',
+        'custumer': list_custumer,
+        'order': list_order,
+        'total_orders': total_order,
+        'delivered': delivered,
+        'pending': pending,
+    }
+    return render(request, 'dashboard.html', data)
