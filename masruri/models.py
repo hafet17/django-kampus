28d74c9c1a423fe656903ae725cb9e23c1d7b23f
Hafet17 <do.crazy192@gmail.com>
2021-02-06 20:59:11 +0700

    add one to one user

--- masruri/models.py
+++ masruri/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -22,10 +23,13 @@ class Student(models.Model):
 
 
 class Custumer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=200, blank=True, null=True)
     phone = models.CharField(max_length=200, blank=True, null=True)
     email = models.CharField(max_length=200, blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    profile_pic = models.ImageField(
+        null=True, blank=True, upload_to='photos/%Y/%m/%d/')
 
     def __str__(self):
         return self.name
