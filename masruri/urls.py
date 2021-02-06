417ca44dac975c8174fa567b12871eb85f17a98d
Hafet17 <do.crazy192@gmail.com>
2021-02-06 21:00:22 +0700

    add urls account

--- masruri/urls.py
+++ masruri/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from  . controllers import dashboard,auth,custumer,product,order
+from . controllers import dashboard, auth, custumer, product, order
 
 urlpatterns = [
     path('', dashboard.home, name='home'),
@@ -8,6 +8,7 @@ urlpatterns = [
     path('update_product/<str:pk>', product.updateProducts, name='update_product'),
     path('delete_product/<str:pk>', product.deleteProducts, name='delete_product'),
     path('custumer/<str:pk>', custumer.index, name='custumer'),
+    path('account/', custumer.accountSetting, name='accountpage'),
     path('create_order/', order.createOrder, name='create_order'),
     path('update_order/<str:pk>', order.updateOrder, name='update_order'),
     path('delete_order/<str:pk>', order.deleteOrder, name='delete_order'),
