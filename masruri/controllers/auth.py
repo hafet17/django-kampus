f07db24c2e3db24bc6215d9fe4dee52c9c78701c
Hafet17 <do.crazy192@gmail.com>
2021-02-06 21:00:53 +0700

    refactor auth controller

--- masruri/controllers/auth.py
+++ masruri/controllers/auth.py
@@ -4,12 +4,13 @@ from masruri.forms import RegisterForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from masruri.decorators import tolakhalaman_ini
+from masruri.decorators import tolakhalaman_ini, ijinkan_pengguna
 from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
 
 
 @tolakhalaman_ini
-def registerPage (request):
+def registerPage(request):
     formregister = RegisterForm()
     if request.method == 'POST':
         formregister = RegisterForm(request.POST)
@@ -19,18 +20,22 @@ def registerPage (request):
             group_custumer = formregister.save()
             grup = Group.objects.get(name='custumer')
             group_custumer.groups.add(grup)
+            Custumer.objects.create(
+                user=group_custumer,
+                name=group_custumer.username)
             # formregister.save()
         return redirect('login')
 
     context = {
-     'judul': 'Halaman Register',
-     'menu': 'register',
-     'tampilregister': formregister
+        'judul': 'Halaman Register',
+        'menu': 'register',
+        'tampilregister': formregister
     }
     return render(request, 'auth/register.html', context)
 
+
 @tolakhalaman_ini
-def loginPage (request):
+def loginPage(request):
     formlogin = AuthenticationForm
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -41,17 +46,29 @@ def loginPage (request):
             login(request, validate)
         return redirect('home')
     context = {
-     'judul': 'Halaman Login',
-     'menu': 'login',
-     'tampillogin': formlogin
+        'judul': 'Halaman Login',
+        'menu': 'login',
+        'tampillogin': formlogin
     }
     return render(request, 'auth/login.html', context)
 
+
+@login_required(login_url='login')
+@ijinkan_pengguna(yang_diizinkan=['custumer'])
 def userPage(request):
-    context = {}
+    order_custumer = request.user.custumer.order_set.all()
+    total_orders = order_custumer.count()
+    delivered = order_custumer.filter(status='Delivered').count()
+    pending = order_custumer.filter(status='Pending').count()
+    context = {
+        'data_order_custumer': order_custumer,
+        'data_total_orders': total_orders,
+        'data_delivered': delivered,
+        'data_pending': pending,
+    }
     return render(request, 'auth/user.html', context)
 
-def logoutPage(request):
-     logout(request)
-     return redirect('login')
 
+def logoutPage(request):
+    logout(request)
+    return redirect('login')
